94d29324ed214b3b0cc5493722aea2388bddaa68
Belal <contactbelalnaim@gmail.com>
2023-03-17 01:10:05 +0530

    Updated router to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import {HashRouter ,Routes,Route} from 'react-router-dom'
 import First from './pages/First';
 import Second from './pages/Second';
 import Navbar from './components/Navbar';
 function App() {
   return (
-    <Router basename='/fastpass'>
+    <HashRouter>
       <Navbar/>
       <Routes>
         <Route path='/' element={<First/>}/>
         <Route path='/second' element={<Second/>}/>
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
